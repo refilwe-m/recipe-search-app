be3c7a3462c3accef98005e3be377628052c684e
Refilwe-M-Mashile <=>
2023-03-18 03:08:30 +0200

    WIP: label + image

--- src/page/searchPage/search-page.tsx
+++ src/page/searchPage/search-page.tsx
@@ -1,13 +1,22 @@
 import React, { useRef, useState } from "react";
+import { getRecipe } from "../../services/recipe-api";
 import "./search-page.scss";
 
 export const SearchPage = () => {
   const [ingredients, setIngredients] = useState<string[]>([]);
+  const [recipe, setRecipe] = useState<string>("");
+  const [recipeImage, setRecipeImage] = useState<string>("");
+
   const list = (items: string[]) => {
     return items.map((item, i) => <li key={i}>{item}</li>);
   };
 
   const inputRef = useRef<HTMLInputElement>(null);
+
+  const fetchReq = async () => {
+    const res = await (await getRecipe(ingredients.join(","))).json();
+    return res;
+  };
 
   return (
     <section id="search-page">
@@ -18,15 +27,26 @@ export const SearchPage = () => {
           <input
             type="button"
             value="Search Recipe"
-            onClick={() => {
+            onClick={async () => {
               setIngredients([...ingredients, inputRef!.current!.value]);
+              const recipeResults = await fetchReq();
+              const recipeLabel = recipeResults?.hits[0]?.recipe?.label;
+              setRecipe(recipeLabel);
+              setRecipeImage(recipeResults?.hits[0]?.recipe?.image);
             }}
           />
         </section>
         <sub>Ingredients</sub>
       </form>
+
       <section id="ingredients">
         <ul>{list(ingredients)}</ul>
+      </section>
+
+      <section>
+        <h3>Recipes</h3>
+        <span>{recipe}</span>
+        <img src={recipeImage} alt="" />
       </section>
     </section>
   );
--- src/services/recipe-api.ts
+++ src/services/recipe-api.ts
@@ -1,5 +1,16 @@
 const BASE_URL = "https://api.edamam.com/api/recipes/v2";
+const APP_ID = import.meta.env.VITE_APP_ID;
+const APP_KEY = import.meta.env.VITE_APP_KEY;
 
-export const getID = (ingredient: string) => {};
-
-console.log(getID("lettuce"));
+export const getRecipe = async (ingredient: string) => {
+  const url = `${BASE_URL}?type=public&q=${ingredient}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "'text/x-typescript",
+    },
+  };
+  const results = await fetch(url, options);
+  return results;
+};
